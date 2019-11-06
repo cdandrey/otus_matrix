f3f32f4163d7ac149293078b88b1c4018d9d7087
Andrey <cdandrey@gmail.com>
2019-11-06 09:26:35 +0200

    Added hand tests

--- test/tb_matrix.cpp
+++ test/tb_matrix.cpp
@@ -5,54 +5,92 @@
 #include <random>
 #include <array>
 
-static const unsigned N{1000};
+const unsigned C_NUM_TEST{1000000};
+const unsigned C_SIZE{1000};
+using arr_t = std::array<int,C_SIZE>;
+using aarr_t = std::array<arr_t,C_SIZE>;
 
-std::random_device rd;
-std::mt19937 gen(rd());
-std::uniform_int_distribution<unsigned> dis_ip(0,255);
+aarr_t arr;
 
 TEST(matrix_test,random_assign_read)
 {
-    //const unsigned C_NUM_TEST{1000000};
-    //const unsigned C_SIZE_MAP{10000};
-    //unsigned op;
-    //unsigned key;
-
-    //std::map<int,int> map_std;
-
-    //std::random_device rd;
-    //std::mt19937 gen(rd());
-    //std::discrete_distribution<unsigned> dis_op = {60,20,20};
-    //std::uniform_int_distribution<unsigned> dis_key(0,C_SIZE_MAP - 1);
-
-    //for (unsigned i = 0; i < C_NUM_TEST; ++i)
-    //{
-    //    op = dis_op(gen);
-    //    key = dis_key(gen);
-
-    //    switch (op)
-    //    {
-    //        // read
-    //        case 0:
-    //            EXPECT_EQ(map_std.emplace(std::make_pair(key,(~key + 1))).second,map_alc.emplace(std::make_pair(key,(~key + 1))).second);
-    //            break;
-
-    //        // assign
-    //        case 1:
-    //            EXPECT_EQ(map_std.erase(key),map_alc.erase(key));
-    //            break;
-    //    }
-    //}
+    unsigned op;
+    unsigned i;
+    unsigned j;
+    int val;
     
-    //auto it_std = map_std.cbegin();
-    //auto it_alc = map_alc.cbegin();
-    //for (; it_std != map_std.cend() && it_alc != map_alc.cend(); ++it_std, ++it_alc)
-    //{
-    //    EXPECT_EQ(it_std->first,it_alc->first);
-    //    EXPECT_EQ(it_std->second,it_alc->second);
-    //}
+    Matrix<int,-1> mtx;
+    for (auto &aa : arr)
+        for (auto &a : aa)
+            a = -1;
 
-    //EXPECT_EQ(map_std.size(), map_alc.size());
+    std::size_t arr_size{0};
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::discrete_distribution<unsigned> dis_op = {70,30};
+    std::uniform_int_distribution<unsigned> dis_idx(0,C_SIZE - 1);
+    std::uniform_int_distribution<int> dis_val;
+
+    for (unsigned t = 0; t < C_NUM_TEST; ++t)
+    {
+        op = dis_op(gen);
+        i = dis_idx(gen);
+        j = dis_idx(gen);
+        val = dis_val(gen);
+
+        switch (op)
+        {
+            // read
+            case 0:
+                EXPECT_EQ(mtx[i][j],arr[i][j]);
+                EXPECT_EQ(mtx.size(),arr_size);
+                break;
+
+            // assign
+            case 1:
+
+                if (arr[i][j] == -1)
+                    ++arr_size;
+
+                arr[i][j] = val;
+                mtx[i][j] = val;
+                EXPECT_EQ(mtx[i][j],arr[i][j]);
+                EXPECT_EQ(mtx.size(),arr_size);
+                break;
+        }
+    }
+    
+    EXPECT_EQ(mtx.size(),arr_size);
+    for (i = 0; i < C_SIZE; ++i)
+        for (j = 0; j < C_SIZE; ++j)
+            EXPECT_EQ(mtx[i][j],arr[i][j]);
+}
+//----------------------------------------------------------------------------------
+
+
+TEST(matrix_test,hand)
+{
+    const int C_DEFAULT_VALUE_V0 = 5;
+    const int C_DEFAULT_VALUE_V5 = 5;
+
+    int tmp;
+    Matrix<int,C_DEFAULT_VALUE_V0> mv0;
+
+    tmp = mv0[100][100];
+    EXPECT_EQ(tmp,C_DEFAULT_VALUE_V0);
+    EXPECT_EQ(mv0.size(),0);
+
+    tmp = 100;
+    mv0[100][100] = tmp;
+    EXPECT_EQ(mv0.size(),1);
+    EXPECT_EQ(mv0[100][100],tmp);
+
+    tmp = mv0[1][1];
+    mv0[1][1] = tmp;
+    EXPECT_EQ(mv0.size(),2);
+    EXPECT_EQ(mv0[1][1],tmp);
+
 }
 //----------------------------------------------------------------------------------
 
